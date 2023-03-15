bb92f4d7cf4ff494296b1db2ad71fb21ecef9a48
ssemenkoff <ssemenkoff@gmail.com>
2023-03-15 13:47:54 +0400

    Multiple selection fixes
    Signed-off-by: ssemenkoff <ssemenkoff@gmail.com>

--- src/components/Modals/FilterModal.vue
+++ src/components/Modals/FilterModal.vue
@@ -13,6 +13,8 @@ export default {
     const reset = () => {
       filterConfigured.value = {};
     };
+    const multipleChoise = ref(false);
+    const currentlySelected = ref(null as any);
     const opened = ({ element }: { element: any }) => {
       rootHierarchy.value = {
         item: element.originalItem,
@@ -20,6 +22,8 @@ export default {
       };
 
       const initialFilters = element.filters;
+      multipleChoise.value = initialFilters.multipleChoise;
+
       if (initialFilters.multipleChoise) {
         filterConfigured.value = {
           enabled: initialFilters.enabled,
@@ -34,6 +38,8 @@ export default {
           multipleChoise: initialFilters.multipleChoise,
           selectedItem: initialFilters.selectedItem,
         };
+
+        currentlySelected.value = initialFilters.selectedItem;
       }
     };
     const { isOpened, run, close } = usePromisifiedModal(reset, opened);
@@ -67,6 +73,8 @@ export default {
           multipleChoise,
           selectedItem,
         };
+
+        currentlySelected.value = selectedItem;
       }
     };
 
@@ -77,11 +85,19 @@ export default {
       close,
       rootHierarchy,
       setSelection,
+      multipleChoise,
+      currentlySelected,
     };
   },
   methods: {
     ok() {
       this.close({ filters: this.filterConfigured });
+    },
+    cancel() {
+      this.close({});
+    },
+    resetSelection() {
+      this.$refs.filterTreeView?.resetSelection();
     },
   },
   components: { FilterTreeView },
@@ -100,13 +116,25 @@ export default {
       <va-card-content>
         <Suspense>
           <FilterTreeView
+            ref="filterTreeView"
             :rootHierarchy="rootHierarchy"
             @set-selection="setSelection"
           ></FilterTreeView>
         </Suspense>
       </va-card-content>
-      <va-card-actions>
-        <va-button @click="ok" color="warning">Ok!</va-button>
+      <va-card-actions class="actions">
+        <div class="action-buttons">
+          <va-button @click="ok" color="primary">Confirm</va-button>
+          <va-button @click="cancel" color="secondary">Cancel</va-button>
+        </div>
+        <div
+          v-if="!multipleChoise && currentlySelected && currentlySelected.id"
+        >
+          <div>Currently selected: {{ currentlySelected.Caption }}</div>
+          <div class="reset-button" @click="resetSelection">
+            Reset selection
+          </div>
+        </div>
       </va-card-actions>
     </template>
   </va-modal>
@@ -143,6 +171,19 @@ export default {
     flex-direction: column;
     overflow: hidden;
     width: 100%;
+  }
+
+  .actions {
+    display: flex;
+    justify-content: space-between !important;
+  }
+
+  .reset-button {
+    margin-top: 0.25rem;
+    color: var(--va-primary);
+    text-decoration: underline;
+    user-select: none;
+    cursor: pointer;
   }
 }
 </style>
--- src/composables/searchResultTreeData.ts
+++ src/composables/searchResultTreeData.ts
@@ -1,38 +1,9 @@
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import { useAppSettingsStore } from "@/stores/AppSettings";
-import { debounce } from "lodash";
 import { optionalArrayToArray } from "@/utils/helpers";
+import { useMultipleSelection } from "./multipleSelection";
 
 const MDDISPINFO_CHILD_COUNT = 65535;
-
-function fillTree(treeNode: any, childMembers: any[]) {
-  const children = childMembers.filter(
-    (member) => member.PARENT_UNIQUE_NAME === treeNode.UName
-  );
-
-  treeNode.id = treeNode.UName;
-
-  if (children.length) {
-    treeNode.children = [...children];
-    treeNode.children.forEach((childNode: any) => {
-      fillTree(childNode, childMembers);
-    });
-
-    treeNode.loaded = true;
-  } else {
-    treeNode.loaded = false;
-
-    const childCount = treeNode.DisplayInfo & MDDISPINFO_CHILD_COUNT;
-    if (childCount) {
-      treeNode.children = [
-        {
-          isLoading: true,
-          id: `Loading_${treeNode.UName}`,
-        },
-      ];
-    }
-  }
-}
 
 function getTreeItem(e: any) {
   const treeItem = {
@@ -65,6 +36,16 @@ export async function useSearchResultTreeData(element: {
   const filteredTree = ref([] as any[]);
   const levels = ref([] as MDSchemaLevel[]);
   const searchValue = ref("");
+  const expanded = ref([] as string[]);
+
+  const {
+    selectAll,
+    selectedItems,
+    deselectedItems,
+    setSelectAll,
+    changeSelection,
+    runCalculations,
+  } = useMultipleSelection(filteredTree, element.filters);
 
   try {
     const hierarchyLevelsResponce = await api.getHierarchyLevels(
@@ -86,43 +67,43 @@ export async function useSearchResultTreeData(element: {
   }
   const searchBy = ref(levels.value[0].LEVEL_UNIQUE_NAME);
 
-  watch(
-    searchValue,
-    debounce(async () => {
-      try {
-        console.log(searchBy.value);
-        let mdx = `
+  const search = async () => {
+    try {
+      console.log(searchBy.value);
+      expanded.value = [];
+      console.log("expanded reset");
+      filteredTree.value = [];
+      let mdx = `
           WITH Set FilteredMembers As 'Head (Filter(AddCalculatedMembers(${element.item.HIERARCHY_UNIQUE_NAME}.Members), InStr(1, ${element.item.HIERARCHY_UNIQUE_NAME}.currentmember.member_caption, "${searchValue.value}")>0),10001)'
           Select
           Hierarchize(Generate(FilteredMembers, Ascendants(${element.item.HIERARCHY_UNIQUE_NAME}.currentmember))) DIMENSION PROPERTIES PARENT_UNIQUE_NAME, MEMBER_TYPE ON 0, {} ON 1 FROM ${element.item.CUBE_NAME}
         `;
-        if (searchBy.value !== "All Members") {
-          mdx = `
+      if (searchBy.value !== "All Members") {
+        mdx = `
             WITH Set FilteredMembers As 'Head (Filter(AddCalculatedMembers(${searchBy.value}.Members), InStr(1, ${element.item.HIERARCHY_UNIQUE_NAME}.currentmember.member_caption, "${searchValue.value}")>0),10001)'
             Select
             Hierarchize(Generate(FilteredMembers, Ascendants(${element.item.HIERARCHY_UNIQUE_NAME}.currentmember))) DIMENSION PROPERTIES PARENT_UNIQUE_NAME, MEMBER_TYPE ON 0, {} ON 1 FROM ${element.item.CUBE_NAME}
           `;
-        }
-        const childMembersResponce = await api.getMDX(mdx);
-        const childMembers = optionalArrayToArray(
-          childMembersResponce.Body.ExecuteResponse.return.root.Axes.Axis[0]
-            .Tuples.Tuple
-        ).map(({ Member }) => Member);
-
-        const tree = childMembers.filter(
-          (member) => !member.PARENT_UNIQUE_NAME
-        );
-
-        tree.forEach((treeNode) => {
-          fillTree(treeNode, childMembers);
-        });
-
-        filteredTree.value = tree;
-      } catch (e) {
-        console.log(e);
       }
-    }, 500)
-  );
+      const childMembersResponce = await api.getMDX(mdx);
+      const childMembers = optionalArrayToArray(
+        childMembersResponce.Body.ExecuteResponse.return.root.Axes.Axis[0]
+          .Tuples.Tuple
+      ).map(({ Member }) => Member);
+
+      const tree = childMembers.filter((member) => !member.PARENT_UNIQUE_NAME);
+
+      tree.forEach((treeNode) => {
+        fillTree(treeNode, childMembers);
+      });
+
+      filteredTree.value = tree;
+
+      runCalculations();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   async function loadChildrenRecursive(treeNode: any, expandedIds: string[]) {
     if (expandedIds.includes(treeNode.id)) {
@@ -145,17 +126,52 @@ export async function useSearchResultTreeData(element: {
         }
       }
 
-      treeNode.children.forEach((childNode: any) => {
-        loadChildrenRecursive(childNode, expandedIds);
+      const openResponces = treeNode.children.map((childNode: any) => {
+        return loadChildrenRecursive(childNode, expandedIds);
       });
+      await Promise.all(openResponces);
+    }
+  }
+
+  function fillTree(treeNode: any, childMembers: any[]) {
+    const children = childMembers.filter(
+      (member) => member.PARENT_UNIQUE_NAME === treeNode.UName
+    );
+
+    treeNode.id = treeNode.UName;
+
+    if (children.length) {
+      treeNode.children = [...children];
+      treeNode.children.forEach((childNode: any) => {
+        fillTree(childNode, childMembers);
+      });
+
+      treeNode.loaded = true;
+      expanded.value.push(treeNode.UName);
+      console.log("expanded set");
+    } else {
+      treeNode.loaded = false;
+
+      const childCount = treeNode.DisplayInfo & MDDISPINFO_CHILD_COUNT;
+      if (childCount) {
+        treeNode.children = [
+          {
+            isLoading: true,
+            id: `Loading_${treeNode.UName}`,
+          },
+        ];
+      }
     }
   }
 
   const triggerExpandedWithSearch = async (expandedIds: string[]) => {
     const currentTreeState = filteredTree.value;
-    currentTreeState.forEach((treeNode: any) => {
-      loadChildrenRecursive(treeNode, expandedIds);
+    const openResponces = currentTreeState.map((treeNode: any) => {
+      return loadChildrenRecursive(treeNode, expandedIds);
     });
+
+    await Promise.all(openResponces);
+    runCalculations();
   };
 
   return {
@@ -163,6 +179,13 @@ export async function useSearchResultTreeData(element: {
     levels,
     searchBy,
     searchValue,
+    expanded,
+    search,
     triggerExpandedWithSearch,
+    searchSelectAll: selectAll,
+    searchSelectedItems: selectedItems,
+    searchDeselectedItems: deselectedItems,
+    searchSetSelectAll: setSelectAll,
+    searchChangeSelection: changeSelection,
   };
 }
