12b07c11bc856099ec98e3c7f95c39c7284ffbf6
vwfox <markus.hochstein@software-hochstein.de>
2023-05-03 22:52:57 +0200

    test workflow
    Signed-off-by: vwfox <markus.hochstein@software-hochstein.de>

---
+++ nightwatch.conf.js
@@ -0,0 +1,196 @@
+// Refer to the online docs for more details:
+// https://nightwatchjs.org/gettingstarted/configuration/
+//
+
+//  _   _  _         _      _                     _          _
+// | \ | |(_)       | |    | |                   | |        | |
+// |  \| | _   __ _ | |__  | |_ __      __  __ _ | |_   ___ | |__
+// | . ` || | / _` || '_ \ | __|\ \ /\ / / / _` || __| / __|| '_ \
+// | |\  || || (_| || | | || |_  \ V  V / | (_| || |_ | (__ | | | |
+// \_| \_/|_| \__, ||_| |_| \__|  \_/\_/   \__,_| \__| \___||_| |_|
+//             __/ |
+//            |___/
+
+module.exports = {
+  // An array of folders (excluding subfolders) where your tests are located;
+  // if this is not specified, the test source must be passed as the second argument to the test runner.
+  test_runner: {
+    type : 'mocha',
+    options : {
+
+        //reporter : 'mocha-json-output-reporter',
+        reporter:'mocha-multi-reporters',
+
+        reporterOptions: {
+          "reporterEnabled": 'mocha-json-output-reporter,mocha-simple-html-reporter',
+          "mochaJsonOutputReporterReporterOptions": {
+            output: './tests_output/axe.json'
+          },
+          "mochaSimpleHtmlReporterReporterOptions":{
+            output:'./tests_output/axe.html'
+          }
+
+        },
+
+    }
+  },
+
+  skip_testcases_on_fail: false,
+  src_folders: ['test','nightwatch'],
+
+  // See https://nightwatchjs.org/guide/concepts/page-object-model.html
+  page_objects_path: [],
+
+  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
+  custom_commands_path: [],
+
+  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
+  custom_assertions_path: [],
+
+  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
+  plugins: ['@nightwatch/vue','nightwatch-axe-verbose'],
+
+  // See https://nightwatchjs.org/guide/concepts/test-globals.html
+  globals_path: '',
+
+  vite_dev_server: {
+    start_vite: true,
+    port: 5173
+  },
+
+  webdriver: {},
+
+  test_workers: {
+    enabled: true
+  },
+
+  test_settings: {
+    default: {
+      disable_error_log: false,
+      launch_url: 'http://localhost:5173',
+
+      screenshots: {
+        enabled: true,
+        path: 'tests_output/screens',
+        on_failure: true
+      },
+
+      desiredCapabilities: {
+        browserName: 'chrome'
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: ''
+      },
+
+    },
+
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        alwaysMatch: {
+          acceptInsecureCerts: true,
+          'moz:firefoxOptions': {
+            args: [
+              // '-headless',
+              // '-verbose'
+            ]
+          }
+        }
+      },
+      webdriver: {
+        start_process: true,
+        server_path: '',
+        cli_args: [
+          // very verbose geckodriver logs
+          // '-vv'
+        ]
+      }
+    },
+
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
+          //
+          // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
+          w3c: true,
+          args: [
+            //'--no-sandbox',
+            //'--ignore-certificate-errors',
+            //'--allow-insecure-localhost',
+            //'--headless'
+          ]
+        }
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: '',
+        cli_args: [
+          // --verbose
+        ]
+      }
+    },
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // Configuration for using remote Selenium service or a cloud-based testing provider.  |
+    //                                                                                     |
+    // Please set the hostname and port of your remote selenium-server or cloud-provider   |
+    // (by setting the following properties in the configuration below):                   |
+    // - `selenium.host`                                                                   |
+    // - `selenium.port`                                                                   |
+    //                                                                                     |
+    // If you are using a cloud provider such as CrossBrowserTesting, LambdaTests, etc.,   |
+    // please set the username and access_key by setting the below environment variables:  |
+    // - REMOTE_USERNAME                                                                   |
+    // - REMOTE_ACCESS_KEY                                                                 |
+    // (.env files are supported)                                                          |
+    ////////////////////////////////////////////////////////////////////////////////////////
+    remote: {
+      // Info on all the available options with "selenium":
+      // https://nightwatchjs.org/guide/configuration/settings.html#selenium-server-settings
+      selenium: {
+        start_process: false,
+        server_path: '',
+        host: '<remote-hostname>',
+        port: 4444
+      },
+
+      username: '${REMOTE_USERNAME}',
+      access_key: '${REMOTE_ACCESS_KEY}',
+
+      webdriver: {
+        keep_alive: true,
+        start_process: false
+      }
+    },
+
+    'remote.chrome': {
+      extends: 'remote',
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          w3c: true
+        }
+      }
+    },
+
+    'remote.firefox': {
+      extends: 'remote',
+      desiredCapabilities: {
+        browserName: 'firefox',
+        'moz:firefoxOptions': {
+          args: [
+            // '-headless',
+            // '-verbose'
+          ]
+        }
+      }
+    },
+
+  },
+
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { viteRequire } from "vite-require";
+import nightwatchPlugin from 'vite-plugin-nightwatch';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteRequire()],
+  plugins: [vue(), viteRequire(),
+    nightwatchPlugin({
+      renderPage: "",
+      componentType: 'vue'
+    })
+
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
