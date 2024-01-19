698984794fd9cf2736728dcffe46982e4a43bf8a
Markus Hochstein <markus.hochstein@software-hochstein.de>
2024-01-19 14:05:23 +0100

    add headers
    fix headers
    remove token
    
    fix headers

--- src/stores/DashboardStore.ts
+++ src/stores/DashboardStore.ts
@@ -1,3 +1,13 @@
+/*
+Copyright (c) 2023 Contributors to the  Eclipse Foundation.
+    This program and the accompanying materials are made
+available under the terms of the Eclipse Public License 2.0
+which is available at https://www.eclipse.org/legal/epl-2.0/
+    SPDX-License-Identifier: EPL-2.0
+
+Contributors: Smart City Jena
+
+*/
 import {defineStore} from "pinia";
 import {ref} from "vue";
 import {getMdxRequest} from "@/utils/MdxRequests/MdxRequestConstructor";
