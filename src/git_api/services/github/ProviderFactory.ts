a378c28d01d4f93f5f7767c07af5238a16911c51
Markus Hochstein <markus.hochstein@software-hochstein.de>
2024-01-19 13:52:35 +0100

    add license headers

--- src/git_api/services/github/ProviderFactory.ts
+++ src/git_api/services/github/ProviderFactory.ts
@@ -1,3 +1,13 @@
+/*
+Copyright (c) 2023 Contributors to the  Eclipse Foundation.
+                   This program and the accompanying materials are made
+                   available under the terms of the Eclipse Public License 2.0
+which is available at https://www.eclipse.org/legal/epl-2.0/
+SPDX-License-Identifier: EPL-2.0
+
+Contributors: Smart City Jena
+
+*/
 import BranchProvider from "@/git_api/services/github/BranchProvider";
 import CommitProvider from "@/git_api/services/github/CommitProvider";
 import FileProvider from "@/git_api/services/github/FileProvider";
