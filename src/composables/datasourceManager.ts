3f9e4db5dece738c714b83fda408f4dfef8e8853
vwfox <markus.hochstein@software-hochstein.de>
2024-07-05 09:41:38 +0200

    premerg

--- src/composables/datasourceManager.ts
+++ src/composables/datasourceManager.ts
@@ -75,11 +75,6 @@ export function useDatasourceManager() {
       }catch (e){
         throw new TypeError(`${ds.type} not found in registry`)
       }
-      if (ds.type === "XMLA") {
-        const datasource = new XmlaDatasource(ds.id, ds.url, ds.caption);
-
-        availableDatasources.value[key] = datasource;
-      }
     });
 
     console.log(availableDatasources.value);
